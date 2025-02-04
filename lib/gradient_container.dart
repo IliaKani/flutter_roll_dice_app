320166d630df657e160cd07c13bb8e60cd330b8a
Ilia Kani <kanishchev.ilia@gmail.com>
2025-02-04 14:49:31 +0100

    init commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topCenter;
+var endAlignment = Alignment.bottomCenter;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
+      ),
+    );
+  }
+}
